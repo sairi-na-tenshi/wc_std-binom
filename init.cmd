c115c39d76d1f50a81bec77058fd85aa0cb0c7b2
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:36:49 +0400

    master

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc_std-binom.git && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc_std-binom.git master && git submodule init && git submodule update
